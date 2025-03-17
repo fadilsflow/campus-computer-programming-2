1cc72e82b8caabf2e2f645938d44b81bf8c920c5
fadilsflow <wahyufadil1140@gmail.com>
2025-03-17 11:39:25 +0700

    pertemuan 3

--- pertemuan-3/src/pertemuan/pkg3/Pertemuan3.java
+++ pertemuan-3/src/pertemuan/pkg3/Pertemuan3.java
@@ -1,20 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pertemuan.pkg3;
 
 /**
  *
  * @author fadils
  */
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import java.io.IOException;
 public class Pertemuan3 {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+  
+        public static void main(String[] args) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+String APIKEY = "1b932005eeba5e1c9bb6560908bdf2c0";
+        Request request = new Request.Builder()
+                .url("https://api.exchangerate.host/live?access_key=1b932005eeba5e1c9bb6560908bdf2c0&currencies=USD,AUD,CAD,PLN,MXN&format=1")
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            System.out.println("Response: " + response.body().string());
+        }
     }
+
     
 }
